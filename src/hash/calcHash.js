cffa38588db716c9c22d5e8ad4cafc1aa7a41551
Iryna <iryna.markovich@vocovo.com>
2022-12-01 11:57:10 +0100

    resolve hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import crypto from 'crypto'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
 
-await calculateHash();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const calculateHash = async () => {
+  const pathToFile = path.resolve(__dirname, 'files/fileToCalculateHashFor.txt')
+
+  fs.readFile(pathToFile, 'utf-8', (e, data) => {
+    const hmac = crypto.createHmac('sha256', data)
+
+    console.log(hmac.digest('hex'))
+  })
+}
+
+await calculateHash()
