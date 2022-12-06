96beb003a1209d923d4dbaf31877a5997057a5ab
Iryna <iryna.markovich@vocovo.com>
2022-12-06 01:09:30 +0100

    resolve cp and wt

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,14 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { fork } from 'child_process'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-spawnChildProcess();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const spawnChildProcess = async (args) => {
+  const pathToFile = path.resolve(__dirname, 'files/script.js')
+
+  fork(pathToFile, args)
+}
+
+await spawnChildProcess()
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,44 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { Worker } from 'node:worker_threads'
+import os from 'os'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-await performCalculations();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const createWorker = (counter) => {
+  const pathToFile = path.resolve(__dirname, 'worker.js')
+
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(pathToFile, {
+      workerData: { counter },
+    })
+
+    worker.on('message', (data) => {
+      resolve({ status: 'resolved', data })
+    })
+    worker.on('error', (err) => {
+      reject({ status: 'error', data: null })
+    })
+  })
+}
+
+const performCalculations = async () => {
+  const cpuCoresQty = os.cpus().length
+  const promisies = []
+
+  let counter = 10
+
+  for (let i = 0; i < cpuCoresQty; i++) {
+    const worker = createWorker(counter)
+    counter++
+
+    promisies.push(worker)
+  }
+
+  const result = await Promise.all(promisies)
+
+  console.log(result)
+}
+
+await performCalculations()
